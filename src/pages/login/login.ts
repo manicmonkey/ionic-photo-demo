2916b18570c9a66990221444441edf2a09fbc0ce
James Baxter <j.w.baxter@gmail.com>
2017-08-29 19:18:26 +0100

    Add login failure handling

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import {Client} from "../../services/rest/client";
-import {NavController} from "ionic-angular";
+import { AlertController, NavController } from "ionic-angular";
 import {HomePage} from "../home/home";
 import {Component, OnInit} from "@angular/core";
 import {UserSession} from "../../app/usersession";
@@ -9,17 +9,27 @@ import {UserSession} from "../../app/usersession";
   templateUrl: 'login.html'
 })
 export class LoginPage implements OnInit {
-  constructor(private client: Client, private navCtrl: NavController, private userSession: UserSession) {
+  constructor(private client: Client, private navCtrl: NavController, private userSession: UserSession, private alertCtrl: AlertController) {
   }
 
-  username: string;
-  password: string;
+  private username: string;
+  private password: string;
 
   login() {
     console.log('Theoretically logging in as ' + this.username);
-    this.client.login('admin', 'password').subscribe(data => {
+    this.client.login(this.username, this.password).subscribe(data => {
       this.userSession.customerNumber = this.username;
       this.navCtrl.setRoot(HomePage);
+    }, err => {
+      console.log('Login failed with error', err);
+      let failureReason = 'Login failed for unknown reasons, please try again';
+      if (err.status == 403)
+        failureReason = 'Login failed due to invalid credentials, please try again';
+      this.alertCtrl.create({
+        title: 'Login failed!',
+        subTitle: failureReason,
+        buttons: ['OK']
+      }).present();
     });
   }
 

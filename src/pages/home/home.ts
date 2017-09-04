7e54a00e4ab753c6c1555e8632a8cae012839f25
James Baxter <j.w.baxter@gmail.com>
2017-09-04 21:37:13 +0100

    Centralise image store

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,8 +13,6 @@ export class HomePage implements OnInit {
   constructor(private client: Client, private userSession: UserSession, private navCtrl: NavController) {
   }
 
-  image: string;
-
   goToCamera() {
     console.log('Going to camera page');
     this.navCtrl.push(CameraPage);
@@ -25,7 +23,7 @@ export class HomePage implements OnInit {
       const id = data[0]['properties']['id'];
       const rev = data[0]['properties']['revision'];
       console.log('Downloading file for document#' + id + ' rev#' + rev);
-      this.image = this.userSession.baseUrl + '/documents/by-id/' + id + '/' + rev + '/file';
+      this.userSession.imageData = this.userSession.baseUrl + '/documents/by-id/' + id + '/' + rev + '/file';
     }, err => {
       console.log('Could not load customer photo');
     });
